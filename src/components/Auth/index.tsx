b426b8c9b18c3e0b01914e44d1dbefb5e9692067
aalout <ilyan.wolf@gmail.com>
2024-07-14 14:55:32 +0300

    TypeScript fixes

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -10,51 +10,51 @@ const Auth = observer(() => {
   const [password, setPassword] = useState('');
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-  const handleSignIn = async (e) => {
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
     if (email === '' || password === '') {
       authStore.error = 'Enter your data';
       return;
     }
-  
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       authStore.error = 'Incorrect email format';
       return;
     }
-  
+
     await authStore.signin(email, password);
     setShowSuccessMessage(true);
     setTimeout(() => {
       setShowSuccessMessage(false);
     }, 1500);
   };
-  
+
   return (
     <div className={styles.body}>
-        <section className={styles.body__section}>
+      <section className={styles.body__section}>
         <h1>Sign in</h1>
-          <form className={styles.body__section__form} onSubmit={handleSignIn}>
-            <input 
-              type="text" 
-              placeholder="Email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input 
-              type="password" 
-              placeholder="Password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {authStore.error && <p className={styles.error}>{authStore.error}</p>}
-            <button type="submit">
-              <span>Sign in</span>
-            </button>
-          </form>
-        </section>
-        {showSuccessMessage && authStore.isLoggedIn && <div className={styles.success}><p className={styles.success__text}>Auth success</p></div>}
+        <form className={styles.bodysectionform} onSubmit={handleSignIn}>
+          <input 
+            type="text" 
+            placeholder="Email" 
+            value={email} 
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          />
+          <input 
+            type="password" 
+            placeholder="Password" 
+            value={password} 
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          />
+          {authStore.error && <p className={styles.error}>{authStore.error}</p>}
+          <button type="submit">
+            <span>Sign in</span>
+          </button>
+        </form>
+      </section>
+      {showSuccessMessage && authStore.isLoggedIn && <div className={styles.success}><p className={styles.success__text}>Auth success</p></div>}
     </div>
   );
 });
