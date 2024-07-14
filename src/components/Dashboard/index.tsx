7c2d3b346517eedaa768bd38f29d29c6f97f9b5d
aalout <ilyan.wolf@gmail.com>
2024-07-14 14:59:09 +0300

    TypeScript fixes

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import styles from "./Dashboard.module.scss"
 
@@ -9,23 +9,31 @@ export default function Dashboard() {
     const [typedText, setTypedText] = useState("");
     const [isTyping, setIsTyping] = useState(false);
     const [cursorIndex, setCursorIndex] = useState(0);
-  
+    const typingIntervalRef = useRef<NodeJS.Timeout | null>(null);
+
     useEffect(() => {
-      let typingInterval;
       if (isTyping) {
-        typingInterval = setInterval(() => {
+        typingIntervalRef.current = setInterval(() => {
           if (typedText.length < textToType.length) {
             setTypedText(textToType.substring(0, typedText.length + 1));
             setCursorIndex(typedText.length + 1);
           } else {
             setIsTyping(false);
-            clearInterval(typingInterval);
+            clearInterval(typingIntervalRef.current!);
           }
         }, 100);
+      } else if (typingIntervalRef.current) {
+        clearInterval(typingIntervalRef.current!);
+        typingIntervalRef.current = null;
       }
-      return () => clearInterval(typingInterval);
+
+      return () => {
+        if (typingIntervalRef.current) {
+          clearInterval(typingIntervalRef.current!);
+        }
+      };
     }, [typedText, isTyping]);
-  
+
     const startTyping = () => {
       setIsTyping(true);
     };
@@ -34,11 +42,10 @@ export default function Dashboard() {
       startTyping();
     }, []);
 
-
   return (
     <div className={styles.body}>
         <section className={styles.body__section}>
-            <p className={styles.body__section__text}>{typedText}<span className={styles.cursor}>|</span></p> 
+            <p className={styles.bodysectiontext}>{typedText}<span className={styles.cursor}>|</span></p> 
         </section>
     </div>
   );
